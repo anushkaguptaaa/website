c5aeaeaae5a0cbc71e7df4de912f1792da4882c4
sewimpractical <anushkagupta100@gmail.com>
2023-05-26 00:56:16 +0530

    Genesis

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Welcome to my internet home. Feel free to look around :)',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
